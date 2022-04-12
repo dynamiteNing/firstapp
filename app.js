8aad1fbdf0f011fb9300cee20fc7fb7ba5f5faa2
EC2 Default User <ec2-user@ip-172-31-14-166.us-west-2.compute.internal>
2022-04-12 22:50:18 +0000

    test

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/getData', getDataRouter);
 
+// test
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
